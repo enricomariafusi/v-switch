a90be7f88481242ba41efcd65820fc2f772d641f
LowEel <e.fusi@reply.de@users.noreply.github.com>
2017-06-17 14:09:18 +0200

    Fixed issue with encryption of frames

--- plane/eth.go
+++ plane/eth.go
@@ -1,6 +1,8 @@
 package plane
 
 import (
+	"V-switch/conf"
+	"V-switch/crypt"
 	"V-switch/tools"
 	"log"
 
@@ -18,15 +20,18 @@ func TapInterpreterThread() {
 	var myframe ethernet.Frame
 	var mymacaddr string
 	var mytlv []byte
+	var encframe []byte
+	var ekey []byte
 
 	for {
 
 		myframe = <-TapToPlane
 		mymacaddr = myframe.Destination().String()
+		ekey = []byte(conf.GetConfigItem("SWITCHID"))
+		encframe = crypt.FrameEncrypt(ekey, myframe)
+		mytlv = tools.CreateTLV("F", encframe)
 
 		if tools.IsMacBcast(mymacaddr) {
-
-			mytlv := tools.CreateTLV("F", myframe)
 
 			for mac, _ := range VSwitch.Ports {
 
@@ -35,7 +40,6 @@ func TapInterpreterThread() {
 
 		} else {
 
-			mytlv = tools.CreateTLV("F", myframe)
 			DispatchTLV(mytlv, mymacaddr)
 
 		}
